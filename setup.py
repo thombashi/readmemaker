d59b8001d76683d1d8af5f66b89b6d3ce74a02ec
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-11-20 19:02:38 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from __future__ import with_statement
 import os.path
 import setuptools
 
@@ -16,7 +15,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="readmemaker",
-    version="0.1.1",
+    version="0.2.0",
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/readmemaker",
@@ -28,7 +27,7 @@ setuptools.setup(
     install_requires=install_requires,
     keywords=["README"],
     long_description=long_description,
-    packages=setuptools.find_packages(exclude=['test*']),
+    packages=setuptools.find_packages(exclude=["test*"]),
     tests_require=tests_require,
     classifiers=[
         "Development Status :: 3 - Alpha",
