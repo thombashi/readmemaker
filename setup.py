c2fdbf3aa81a8dacf976e6d531af234c291576af
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-22 00:51:25 +0900

    Add extras_require

--- setup.py
+++ setup.py
@@ -59,6 +59,10 @@ setuptools.setup(
 
     install_requires=install_requires,
     tests_require=tests_require,
+    extras_require={
+        "build": "wheel",
+        "release": "releasecmd>=0.0.8",
+    },
 
     classifiers=[
         "Development Status :: 3 - Alpha",
