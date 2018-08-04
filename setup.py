33ff1a3d64cca539b1c964729221b193119bda65
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-08-04 15:45:38 +0900

    Add Python 3.7 to classifiers

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Documentation",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
