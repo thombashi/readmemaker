9c958fb8af06a9413c11968c96794ffa691461e8
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2018-08-04 15:40:49 +0900

    Apply black

--- readmemaker/__init__.py
+++ readmemaker/__init__.py
@@ -50,8 +50,7 @@ class ReadmeMaker(object):
 
         self.__project_name = project_name
         self.__indent_level = 0
-        self.__stream = io.open(
-            os.path.join(output_dir, "README.rst"), "w", encoding=encoding)
+        self.__stream = io.open(os.path.join(output_dir, "README.rst"), "w", encoding=encoding)
 
         self.__encoding = encoding
 
@@ -75,45 +74,39 @@ class ReadmeMaker(object):
         self.__indent_level -= 1
 
     def write_line_list(self, line_list, line_break_count=2):
-        self.__stream.write("\n".join([
-            self.__adjust_for_pypi(line)
-            for line in line_list
-            if re.search(":caption:", line) is None
-        ]))
+        self.__stream.write(
+            "\n".join(
+                [
+                    self.__adjust_for_pypi(line)
+                    for line in line_list
+                    if re.search(":caption:", line) is None
+                ]
+            )
+        )
         self.__stream.write("\n" * line_break_count)
 
     def write_toc(self, header="Table of Contents"):
-        self.write_line_list([
-            ".. contents:: {:s}".format(header),
-            "   :depth: 2",
-        ])
+        self.write_line_list([".. contents:: {:s}".format(header), "   :depth: 2"])
 
     def write_chapter(self, text):
-        self.write_line_list([
-            text,
-            self.__get_chapter_char() * (len(text) + 2),
-        ], line_break_count=1)
+        self.write_line_list(
+            [text, self.__get_chapter_char() * (len(text) + 2)], line_break_count=1
+        )
 
     def write_file(self, file_path):
         with io.open(file_path, "r", encoding=self.__encoding) as f:
-            self.write_line_list(
-                [line.rstrip() for line in f.readlines()])
+            self.write_line_list([line.rstrip() for line in f.readlines()])
 
     def write_introduction_file(self, filename):
         self.write_file(
-            self.doc_page_root_dir_path.joinpath(self.introduction_dir_name)
-                .joinpath(filename))
+            self.doc_page_root_dir_path.joinpath(self.introduction_dir_name).joinpath(filename)
+        )
 
     def write_from_relative_file(self, filename):
         self.write_file(self.file_search_dir_path.joinpath(filename))
 
     def __get_chapter_char(self):
-        char_table = {
-            0: "=",
-            1: "-",
-            2: "~",
-            3: "^",
-        }
+        char_table = {0: "=", 1: "-", 2: "~", 3: "^"}
 
         return char_table.get(self.__indent_level, char_table[max(char_table)])
 
