bd6c97186f404892de2472155b080c0d6a6c62aa
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-02 19:11:18 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setuptools.setup(
     },
 
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
