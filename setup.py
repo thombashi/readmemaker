b665906655237238f14d190fcceb675ab2a3c61f
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2021-05-03 17:20:42 +0900

    Add support for Python 3.10

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Documentation",
         "Topic :: Software Development :: Documentation",
         "Topic :: Software Development :: Libraries",
